31c70797475aa382f5af9f87162be4add2107eb3
sushantlp <sushantsingh.1081@gmail.com>
2019-03-14 11:56:20 +0530

    Migration and Model

--- migrations/20190313133226-create-invoice.js
+++ migrations/20190313133226-create-invoice.js
@@ -37,15 +37,65 @@ module.exports = {
           key: 'store_id'
         }
       },
+      customer_name: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      customer_mobile: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      membership_code: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      total_amount: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0
+      },
+      invoice_cashback: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0
+      },
+      invoice_total_saving: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0
+      },
+      invoice_loyalty_used: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0
+      },
+      invoice_total_amount: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0
+      },
+      gstin_name: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      gstin_number: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      return_status: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: 0
+      },
+      round_off_amount: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0
+      },
       status: {
         type: Sequelize.BOOLEAN,
         defaultValue: 0
       },
       createdAt: {
+        field: "created_at",
         allowNull: false,
         type: Sequelize.DATE
       },
       updatedAt: {
+        field: "updated_at",
         allowNull: false,
         type: Sequelize.DATE
       }
--- models/invoice.js
+++ models/invoice.js
@@ -1,7 +1,23 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var invoice = sequelize.define('invoice', {
-    firstName: DataTypes.STRING
+    invoice_no: DataTypes.INTEGER,
+    store_counter_id: DataTypes.INTEGER,
+    warehouse_user_id: DataTypes.INTEGER,
+    store_id: DataTypes.INTEGER,
+    customer_name: DataTypes.STRING,
+    customer_mobile: DataTypes.INTEGER,
+    membership_code: DataTypes.STRING,
+    total_amount: DataTypes.FLOAT,
+    invoice_cashback: DataTypes.FLOAT,
+    invoice_total_saving: DataTypes.FLOAT,
+    invoice_loyalty_used: DataTypes.FLOAT,
+    invoice_total_amount: DataTypes.FLOAT,
+    gstin_name: DataTypes.STRING,
+    gstin_number: DataTypes.STRING,
+    return_status: DataTypes.BOOLEAN,
+    round_off_amount: DataTypes.FLOAT,
+    status: DataTypes.BOOLEAN
   }, {});
   invoice.associate = function(models) {
     // associations can be defined here
