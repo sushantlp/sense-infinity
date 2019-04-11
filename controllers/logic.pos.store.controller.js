95640c58e20a6b76f732e447537437cac2c8a161
sushantlp <sushantsingh.1081@gmail.com>
2019-04-11 14:30:17 +0530

    Pos store prdouct api complete

--- controllers/logic.pos.store.controller.js
+++ controllers/logic.pos.store.controller.js
@@ -292,7 +292,7 @@ const getWarehouseProduct = async(sync, mobile) => {
     const quesmarks = new Array(attribute.length).fill('?').join(',');
 
     // Read Warehouse Product Record By Array
-    const productRecord = await databaseController.readWarehouseProductArray("*", mobile, quesmarks, attribute);
+    const productRecord = await databaseController.readWarehouseProductArray(`product_barcode AS barcode, IFNULL(product_name,'') as product_name, IFNULL(brand_name,'') as brand_name, IFNULL(description,'') as description, global_category_id AS category_id, global_sub_category_id AS sub_category_id, global_sub_sub_category_id AS sub_sub_category_id, product_unit_id AS unit_id, product_sub_unit_id AS sub_unit_id, product_size, selling_price, product_margin, actual_price, product_quantity, sgst, cgst, igst, hsn, sodexo, status`, mobile, quesmarks, attribute);
 
     if (productRecord === 0) return {
       success: false,
